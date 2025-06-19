c43cb6ab4e46496cf7fed978e156162e2d003813
YaoXinDaple <dapleyx@gmail.com>
2025-06-19 22:42:28 +0800

    add builder pattern regular example and real world example projects

---
+++ BuilderPatternRegularExample/Address.cs
@@ -0,0 +1,13 @@
+﻿namespace BuilderPattern
+{
+    public class Address
+    {
+        internal Address()
+        {
+        }
+        public string Street { get; init; } = string.Empty;
+        public string Detail { get; init; } = string.Empty;
+        public string Province { get; init; } = string.Empty;
+        public string ZipCode { get; init; } = string.Empty;
+    }
+}
---
+++ BuilderPatternRegularExample/AddressBuilder.cs
@@ -0,0 +1,45 @@
+﻿namespace BuilderPattern
+{
+    public class AddressBuilder
+    {
+        public static AddressBuilder Empty() => new();
+        private string? _street;
+        private string? _detail;
+        private string? _province;
+        private string? _zipCode;
+
+        private AddressBuilder()
+        {
+        }
+        public AddressBuilder WithStreet(string street)
+        {
+            _street = street;
+            return this;
+        }
+        public AddressBuilder WithDetail(string detail)
+        {
+            _detail = detail;
+            return this;
+        }
+        public AddressBuilder WithProvince(string province)
+        {
+            _province = province;
+            return this;
+        }
+        public AddressBuilder WithZipCode(string zipCode)
+        {
+            _zipCode = zipCode;
+            return this;
+        }
+        public Address Build()
+        {
+            return new Address
+            {
+                Street = _street ?? string.Empty,
+                Detail = _detail ?? string.Empty,
+                Province = _province ?? string.Empty,
+                ZipCode = _zipCode ?? string.Empty
+            };
+        }
+    }
+}
